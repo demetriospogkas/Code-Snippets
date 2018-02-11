42eac443090a4bb8c1674247dec8a1027356fdde
demetriospogkas <demetriospogkas@gmail.com>
2018-02-11 11:45:52 -0500

    js get element attrs

--- javascript-snippets.js
+++ javascript-snippets.js
@@ -6,5 +6,14 @@ d3.select({element})
     .value
 
 // JS - Get the last element of an array
+{array}[{array}.length - 1]
+
+//JS - Slice an array
 {array}
-  .slice(1, {array}.length - 1)
+  .slice(1, ({array}.length)) // Slice array from second to last element
+
+{array}
+  .slice(0, ({array}.length - 1)) // Slice array from first to second to last element
+
+{array}
+  .slice(1, -1) // Slice array from second to second to last element
