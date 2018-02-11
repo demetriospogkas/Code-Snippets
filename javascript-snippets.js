0c91495aaa3bc05b37de5e4cd97783425e14b335
demetriospogkas <demetriospogkas@gmail.com>
2018-02-11 11:01:18 -0500

    js get element attrs

---
+++ javascript-snippets.js
@@ -0,0 +1,10 @@
+// D3 - Get an HTML element's attributes
+d3.select({element})
+    .node()
+    .attributes
+    .{attribute}
+    .value
+
+// JS - Get the last element of an array
+{array}
+  .slice(1, {array.length - 1})
