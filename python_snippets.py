c897ce874ba986b43dada4152f28f5b4ff1d62f5
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-08-09 13:49:44 -0400

    bad utf-8 encoding

--- python_snippets.py
+++ python_snippets.py
@@ -1,5 +1,5 @@
 ### Locate bad UTF-8 encoding
-### Kudos: 
+### Kudos: https://stackoverflow.com/questions/46180610/python-3-unicodedecodeerror-how-do-i-debug-unicodedecodeerror
 >>> file = {file_dir}
 UnicodeDecodeError: 'utf-8' codec can't decode byte {byte} in position {position}: invalid continuation byte
 
