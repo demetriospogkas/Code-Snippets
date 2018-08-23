3a7c6836441a9aa6dc8e6727bc9459beb1d90f24
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-08-23 18:43:46 -0400

    regex groups sub

--- python_snippets.py
+++ python_snippets.py
@@ -1,12 +1,16 @@
 ### Locate bad UTF-8 encoding
 ### Kudos: https://stackoverflow.com/questions/46180610/python-3-unicodedecodeerror-how-do-i-debug-unicodedecodeerror
->>> file = {file_dir}
-UnicodeDecodeError: 'utf-8' codec can't decode byte {byte} in position {position}: invalid continuation byte
+>>> file = ${file_dir}
+UnicodeDecodeError: 'utf-8' codec can't decode byte ${byte} in position ${position}: invalid continuation byte
 
 >>> with open(file, 'r') as inputFile:
 >>>    data = inputFile.read()
 
 >>> from pprint import pprint
 >>> f = open(file, 'rb')
->>> f.seek({position})
+>>> f.seek(${position})
 >>> pprint(f.read(500))
+
+### Regex groups substitution
+import re
+re.sub('(\d{2})/(\d{2})/(\d{4})', '\g<3>-\g<1>-\g<2>', ${a_date_string})
