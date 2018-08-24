18a4455ad7c273d0abb7e237302d05bc9dc05ddb
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-08-24 12:42:17 -0400

    pandas groupby three levels

--- python_snippets.py
+++ python_snippets.py
@@ -14,3 +14,10 @@ UnicodeDecodeError: 'utf-8' codec can't decode byte ${byte} in position ${positi
 ### Regex groups substitution
 import re
 re.sub('(\d{2})/(\d{2})/(\d{4})', '\g<3>-\g<1>-\g<2>', ${a_date_string})
+
+### Pandas groupby and value_counts with three columns
+### Kudos: https://stackoverflow.com/questions/42998660/pandas-how-to-groupby-with-count-with-multiple-levels-on-rows
+import pandas as pd
+df = pd.DataFrame(...)
+df.groupby([${col1}, ${col2}, ${col3}]).size().unstack().stack(dropna=True).reset_index(name="Count")
+
