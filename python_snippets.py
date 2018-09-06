7e7562aa98100e27d790e99de520ef0336642cbd
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-09-06 10:14:14 -0400

    jupyter notebook window width

--- python_snippets.py
+++ python_snippets.py
@@ -24,3 +24,8 @@ df.groupby([${col1}, ${col2}, ${col3}]).size().unstack().stack(dropna=True).rese
 ### Pandas sort by multiple columns with multiple different ascending values
 ### Kudos: https://stackoverflow.com/questions/17141558/how-to-sort-a-dataframe-in-python-pandas-by-two-or-more-columns
 df.sort_values(by=[${col1}, ${col2}, ${col3}], ascending=[${True/False}, ${True/False}, ${True/False}], inplace=True)
+
+### Increase Jupyter Notebook window size
+### Kudos: https://stackoverflow.com/questions/21971449/how-do-i-increase-the-cell-width-of-the-jupyter-ipython-notebook-in-my-browser/34058270#34058270
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:70% !important; }</style>"))
