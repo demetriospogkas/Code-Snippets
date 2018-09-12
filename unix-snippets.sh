36ac384f40a5564b332892400d94903c654d0e44
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-09-12 11:20:03 -0400

    unix add exec rights to file

--- unix-snippets.sh
+++ unix-snippets.sh
@@ -5,3 +5,7 @@ ls -l .
 ### Check admin right in file
 cd $(DIR)
 ls -l $(FILE)
+
+### Add to admin rights of file to make it executable
+### More: https://askubuntu.com/questions/932713/what-is-the-difference-between-chmod-x-and-chmod-755
+chmod +x $(FILE)
