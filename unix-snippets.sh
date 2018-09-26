896e79411ede8cdb733c84005f85415cc7a70847
Demetrios Pogkas <demetriospogkas@gmail.com>
2018-09-26 16:55:25 -0400

    unix look for text in files

--- unix-snippets.sh
+++ unix-snippets.sh
@@ -9,3 +9,7 @@ ls -l $(FILE)
 ### Add to admin rights of file to make it executable
 ### More: https://askubuntu.com/questions/932713/what-is-the-difference-between-chmod-x-and-chmod-755
 chmod +x $(FILE)
+
+### Look for text in files
+### Kudos: https://stackoverflow.com/questions/16956810/how-do-i-find-all-files-containing-specific-text-on-linux
+grep -rn $(DIRECTORY) -e '$(PATTERN)'
